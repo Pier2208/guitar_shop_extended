4988bacd2bf6d950c2885e774456077e61001210
Pierrick Le Roy <p.leroy.mtl@gmail.com>
2018-12-23 15:37:56 -0500

    connected to mongoDB with native driver

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+require('dotenv').config()
+
 
 //init app
 const app = express()
+
+
+//mongoDB connect
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true })
+client.connect()
+    .then( client => console.log('Connected to mongoDB'))
+    .catch(err => console.log('Something webt wrong', err))
+client.close()
+
 
 //listening to
 const PORT = process.env.PORT || 5000
